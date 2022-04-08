6a4fd20f47f886735896af091df76728cc8bcffe
Zoe-Codes <hello.zoecodes@gmail.com>
2022-04-07 18:19:14 -0700

    Task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Hello, printf
+ *Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece if art,\n");
+return (0);
+}
