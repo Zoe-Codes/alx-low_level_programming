26fbac2a90e9c5dcc46434c7033442afab8efc38
Zoe-Codes <hello.zoecodes@gmail.com>
2022-04-11 20:39:09 -0700

    Task 12 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Print all possile combinations of 2-digit numbers
+ * main - Print all possible combinations of 2-digit numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
@@ -15,12 +15,12 @@ for (j = 0; j < 100; j++)
 {
 if (i < j)
 {
-putchar((i /10) + 48);
+putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
 putchar((j / 10) + 48);
 putchar((j % 10) + 48);
-if (i !=98 || j !=99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
