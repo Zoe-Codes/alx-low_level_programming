bfd8deff5f43f7b72650a2d8f28606314b00bec9
Zoe-Codes <hello.zoecodes@gmail.com>
2022-04-12 16:40:14 -0700

    Print the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main- Print the alphabe, in lowercase, followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
