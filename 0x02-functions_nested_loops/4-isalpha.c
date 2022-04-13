ed12ce7de51285998df8a70b1feb50c78d318426
Zoe-Codes <hello.zoecodes@gmail.com>
2022-04-12 18:19:08 -0700

    Checks for letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Check for alphabetic character.
+ * @c: the int to print 
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
