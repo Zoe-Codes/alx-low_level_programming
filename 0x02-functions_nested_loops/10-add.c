e7cee2072726ff443e0a1e2c5c366eb132fc5b1f
Zoe-Codes <hello.zoecodes@gmail.com>
2022-04-12 19:21:02 -0700

    Add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Add two integers and returns the result.
+ * @i: print int i
+ * @k: print int k
+ * Return: Always 0 (Success)
+ */
+int add(int i, int k)
+{
+return (i + k);
+}
