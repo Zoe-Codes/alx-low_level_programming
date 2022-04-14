9d9a44da2c5091c011315e414f07ace40450d391
Zoe-Codes <hello.zoecodes@gmail.com>
2022-04-13 19:53:17 -0700

    Finds and prints even-valued terms

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first 50 Fibonacci numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+long j = 1, k = 2;
+
+while (i < 50)
+{
+if (i == 0)
+printf ("%ld", j);
+else if (i == 1)
+
